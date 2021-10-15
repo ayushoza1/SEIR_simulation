6b19a44f22c13218371d28c0f5b2ceec48353dd9
ayushoza1 <a.s.oza@ed.ac.uk>
2021-10-15 14:00:49 +0100

    Updated Comments

--- SEIR_Simmulation.R
+++ SEIR_Simmulation.R
@@ -1,10 +1,37 @@
-## 0 = Susecptible ## 1 = Exposed ## 2 = Infected ## 3 = Recovered/Dead 
-## x is all of the people 
+## SEIR Model
+
+## Overview
+## ********
+## The SEIR model is a model to recreate the trajectory of a epidemic, in particular following the path of infections for 3 cohorts. 
+## The whole population, the 'cautious' 10% of the population defined as 10% of the population with the lowest beta values and a 
+## random sample of 0.1% of the population. Defining 4 states, susceptible, exposed, infected and recovered or dead on each day 
+## an individual moves from suseptible state to the exposed state a function of their contact rate, the infected populations 
+##contact rate and a viral infectivity parameter lamda. Each day an individual can move from being exposed to infected with 
+## the disease with probability 1/3 and move from infected to recovered or dead with probability 1/5
+
+## SEIR model details
+## ******************
+## 1. Vectors are initiated whereby 'x' is defined as the storage vector for the whole population each element can either be 
+##    0 = Susecptible ## 1 = Exposed ## 2 = Infected ## 3 = Recovered/Dead 
+## 2. Vectors are initiated whereby 'S', 'E', 'I', 'R' are vectors showing how many people are in each state each day
+## 3. Beta's are assigned to each individual (using rnom) and stored in 'beta' the bottom 10% is identified and stored
+## 4. A random saample of 0.1% is also taken from the population for use later (using sample)
+## 5. Model starts by using uniform random devidates (runif) to move people from state E -> I and E -> R
+## 5. A person, i,  in state S are assigned a probabilty of moving to state E by sum_infectious * lamda * beta(i) and 
+##    moved to state S based on random deviates (runif)
+## 6. Three count vectors are set up, new_infections, new_infectios_percentile, new_infections_sample and the number of people 
+##    moving from E -> I in each group is recorded for that dat
+## 7. Model runs for 150 days
+## 8. Plots are created using plot, lines, mtext, ablines and legend functions
 
 seir <-function(n=5500000,ne=10,nt=150) {
   ## SEIR stochastic simulation model.
   ## n = population size; ne = initially exposed; nt = number of days
-  ## gamma = daily prob E -> I; delta = daily prob I -> R;
+  ##Sum of betas of indivial in infected state * beta of susepital individual * landa is prob S -> E
+  ## 1/3 prob of moving E -> I
+  ## 1/5 prob of moving I -> S with the assumption this can happen after 1 day
+  ## 0 = Susecptible ## 1 = Exposed ## 2 = Infected ## 3 = Recovered/Dead 
+  
   lamda <- 0.4/n
   x <-rep(0,n) ## initialize to susceptible state
   beta <-rlnorm(n,0,0.5); beta <-beta/mean(beta) ## individual infection rates
@@ -22,8 +49,8 @@ seir <-function(n=5500000,ne=10,nt=150) {
   
   for (i in 2:nt) { ## loop over days
     
-    sum_infectious = sum(beta[x == 2])
-    prob_exposed =  sum_infectious * lamda * beta
+    sum_infectious = sum(beta[x == 2]) 
+    prob_exposed =  sum_infectious * lamda * beta 
     
     u <-runif(n) ## uniform random deviates 
     
@@ -69,7 +96,6 @@ legend(1, 2, legend=c("Whole population", "Cautious 10% of the popultion", "Rand
        col=c("brown", "green", "cadetblue"), lty=1:1, cex=0.5)
 abline(v = c(maxtrfxx, maxtrfyy, maxtrfzz) , lty=c(2, 2, 2), lwd=c(0.4, 0.4, 0.4), col=c("grey", "grey"))
 mtext(maxtrfxx, side = 1, at = maxtrfxx, cex = 0.7) ; mtext(maxtrfyy, side = 1, at = maxtrfyy, cex = 0.7) ; mtext(maxtrfzz, side = 1, at = maxtrfzz, cex = 0.7) 
-
 
 #Gowtham
 
